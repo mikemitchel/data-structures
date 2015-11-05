54f88eb6dc6f072cc3a631ee19c152bb72a3a6f6
MKS-students <mks.students@hackreactor.com>
2015-11-05 11:25:53 -0600

    psuedoclassical stack complete

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.topItem = 0;
+
 };
+
+Stack.prototype.push = function (value) {
+  this.storage[this.topItem] = value;
+  this.topItem++;
+}
+
+Stack.prototype.pop = function () {
+  this.topItem--
+  var hold = this.storage[this.topItem];
+  delete this.storage[this.topItem];
+  return hold;
+}
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+}
 
 
