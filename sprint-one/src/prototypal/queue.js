4bb4803ada68b6f9b249f890d87ee09cfbb33c07
MKS-students <mks.students@hackreactor.com>
2015-11-05 11:13:43 -0600

    prototypal queue complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instanceOf = Object.create(queueMethods);
+  instanceOf.storage = {};
+  instanceOf.nextInLine = 0;
+  instanceOf.lastInLine = 0;
+
+  return instanceOf;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.lastInLine] = value;
+    this.lastInLine++
+  },
+  dequeue: function() {
+    var hold = this.storage[this.nextInLine];
+    delete this.storage[this.nextInLine];
+    this.nextInLine++;
+    return hold;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
