f918c5ae45372157f4f5c1080ae326cf2d92bd6b
MKS-students <mks.students@hackreactor.com>
2015-11-06 14:00:56 -0600

    tree.js complete using _.reduce

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,14 +15,40 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+  // var answer = false;
+  // if (this.value === target) {
+  //   return true;
+  // } else {
+  //   _.each(this.children, function(child) {
+  //      if (child.contains(target)) {
+  //        answer = true;
+  //      }
+  //   });
+  //   return answer;
+  // }
+  if (this.value === target) {
+    return true;
+  } else {
+    return _.reduce(this.children, function(answer, child) {
+      if (answer) {
+        return true;
+      }
+      return child.contains(target);
+    }, false)
+  }
+
 
 };
+
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *  .addChild O(1) and .contains is  O(n)
  */
