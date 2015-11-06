a17cd284f789aaf36e14dbe4d8b67126d7869b1b
MKS-students <mks.students@hackreactor.com>
2015-11-06 14:38:20 -0600

    binarySearchTree insert working ß≈

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,42 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(biTreeMethods);
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+  tree.dupes = 0;
+
+
+  return tree;
 };
+
+var biTreeMethods = {};
+
+biTreeMethods.insert = function(value) {
+
+  if (value === this.value) {
+    this.dupes++;
+  } else if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
+
+biTreeMethods.contains = function(target) {
+
+}
+
+biTreeMethods.depthFirstLog = function(cb) {
+
+}
 
 
 /*
