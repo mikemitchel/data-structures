78a745d49b677abac7e236159e9af6651eac079f
MKS-students <mks.students@hackreactor.com>
2015-11-07 11:38:20 -0600

    hashTable complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,25 @@
  var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._collisions = {};
 };
 
 HashTable.prototype.insert = function(key, value){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  //push the k/v pair into the bucket[index]
-  if (this.retrieve(key) !== value) {
+  if (this._storage[i] !== value && this._storage[i] !== null && this._storage[i] !== undefined) {
+    this._collisions[key] = value;
+  } else {
     this._storage[i] = value;
   }
-
 };
 
 HashTable.prototype.retrieve = function(key){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  return this._storage[i];
-
+  if (this._collisions.hasOwnProperty(key)) {
+    return this._collisions[key];
+  } else {
+    return this._storage[i];
+  }
 };
 
 HashTable.prototype.remove = function(key){
@@ -24,7 +28,13 @@ HashTable.prototype.remove = function(key){
 };
 
 
-
 /*
  * Complexity: What is the time complexity of the above functions?
+ HashTable constructor is O(1),
+ insert is amortized constant O(1) (when buckets are full/threshold is met),
+ retrieve is O(1),
+ remove is O(1)
  */
+
+
+
