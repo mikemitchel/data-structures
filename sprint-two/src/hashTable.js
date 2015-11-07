623addb0609b54f2bbc01387d2c7e5ce019a1b7c
MKS-students <mks.students@hackreactor.com>
2015-11-06 19:43:42 -0600

    insert, remove, and retrive working for hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,23 @@
-var HashTable = function(){
+ var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  //push the k/v pair into the bucket[index]
+  this._storage[i] = value;
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  return this._storage[i];
 
 };
 
-HashTable.prototype.remove = function(k){
-
+HashTable.prototype.remove = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  this._storage[i] = null;
 };
 
 
